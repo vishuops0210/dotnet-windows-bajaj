88379eceb2e181be89ab896ac7477ee8fc218f8b
vishal <vt3630@gmail.com>
2026-07-21 09:25:13 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,16 +1,40 @@
 using System;
+using System.Net;
+using System.Text;
+using System.Threading;
 
-namespace BajajDemo
+namespace App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello from Bajaj Capital .NET 4.7 Demo!");
-            
-            // Deliberate SonarQube violation: hardcoded password
-            string dbPassword = "SuperSecretPassword123!";
-            Console.WriteLine("Connecting to DB...");
+            int port = 8080;
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add($"http://localhost:{port}/");
+            listener.Start();
+            Console.WriteLine($"Listening on port {port}...");
+
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+
+                string responseString = @"
+                    <html>
+                      <body style='font-family: sans-serif; text-align: center; margin-top: 50px;'>
+                        <h1>Welcome to .NET 4.7 App!</h1>
+                        <p>This is a fully functional backend API ready for the demo.</p>
+                      </body>
+                    </html>";
+                
+                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
         }
     }
 }
